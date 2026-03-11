053a0ed21d023c9a11a733ff4326c5a653f56e11
Shrouk <selkassas17@gmail.com>
2026-03-11 19:38:57 +0200

    tables creation

---
+++ CreateDbTables.sql
@@ -0,0 +1,107 @@
+CREATE DATABASE ITI_ExaminationDB;
+
+USE ITI_ExaminationDB;
+
+CREATE TABLE Branch (
+    BranchID INT PRIMARY KEY IDENTITY(1,1),
+    BranchName NVARCHAR(100) NOT NULL,
+    Location NVARCHAR(200)
+);
+
+CREATE TABLE Track (
+    TrackID INT PRIMARY KEY IDENTITY(1,1),
+    TrackName NVARCHAR(100) NOT NULL,
+    BranchID INT FOREIGN KEY REFERENCES Branch(BranchID) ON DELETE CASCADE,
+    DurationMonths INT
+);
+
+CREATE TABLE Course (
+    CourseID INT PRIMARY KEY IDENTITY(1,1),
+    CourseName NVARCHAR(100) NOT NULL,
+    MinDegree INT,
+    MaxDegree INT
+);
+
+CREATE TABLE Track_Course (
+    TrackID INT FOREIGN KEY REFERENCES Track(TrackID) ON DELETE CASCADE,
+    CourseID INT FOREIGN KEY REFERENCES Course(CourseID)ON DELETE CASCADE,
+    CONSTRAINT PK_TrackCourse PRIMARY KEY (TrackID, CourseID)
+);
+
+CREATE TABLE Instructor (
+    InstructorID INT PRIMARY KEY IDENTITY(1,1),
+    InstructorName NVARCHAR(100) NOT NULL,
+    Email NVARCHAR(255) UNIQUE,
+    DepartmentNo INT 
+);
+
+CREATE TABLE Instructor_Course (
+    InstructorID INT FOREIGN KEY REFERENCES Instructor(InstructorID) ON DELETE CASCADE,
+    CourseID INT FOREIGN KEY REFERENCES Course(CourseID) ON DELETE CASCADE,
+    CONSTRAINT PK_InstructorCourse PRIMARY KEY (InstructorID, CourseID) 
+);
+
+CREATE TABLE Student (
+    StudentID INT PRIMARY KEY IDENTITY(1,1),
+    StudentName NVARCHAR(100) NOT NULL,
+    Email NVARCHAR(255) UNIQUE,
+    Phone NVARCHAR(20) 
+);
+
+CREATE TABLE Student_Track (
+    StudentID INT FOREIGN KEY REFERENCES Student(StudentID) ON DELETE CASCADE,
+    TrackID INT FOREIGN KEY REFERENCES Track(TrackID) ON DELETE CASCADE,
+    CONSTRAINT PK_StudentTrack PRIMARY KEY (StudentID, TrackID) 
+);
+
+CREATE TABLE Question (
+    QuestionID INT PRIMARY KEY IDENTITY(1,1),
+    CourseID INT FOREIGN KEY REFERENCES Course(CourseID) ON DELETE CASCADE,
+    QuestionText NVARCHAR(MAX) NOT NULL,
+    QuestionType NVARCHAR(10) CHECK (QuestionType IN ('MCQ', 'TF')),
+    Points INT 
+);
+
+CREATE TABLE [Option] (
+    OptionID INT PRIMARY KEY IDENTITY(1,1),
+    QuestionID INT FOREIGN KEY REFERENCES Question(QuestionID) ON DELETE CASCADE,
+    OptionText NVARCHAR(MAX) NOT NULL,
+    OptionOrder INT
+);
+
+CREATE TABLE ModelAnswer (
+    ModelAnswerID INT PRIMARY KEY IDENTITY(1,1),
+    QuestionID INT UNIQUE FOREIGN KEY REFERENCES Question(QuestionID) ON DELETE CASCADE,
+    OptionID INT FOREIGN KEY REFERENCES [Option](OptionID)
+);
+
+CREATE TABLE Exam (
+    ExamID INT PRIMARY KEY IDENTITY(1,1),
+    ExamName NVARCHAR(150),
+    CourseID INT FOREIGN KEY REFERENCES Course(CourseID),
+    CreatedDate DATETIME DEFAULT GETDATE(),
+    TotalQuestions INT 
+);
+
+CREATE TABLE Exam_Question (
+    ExamID INT FOREIGN KEY REFERENCES Exam(ExamID) ON DELETE CASCADE,
+    QuestionID INT FOREIGN KEY REFERENCES Question(QuestionID),
+    OrderNo INT,
+    CONSTRAINT PK_ExamQuestion PRIMARY KEY (ExamID, QuestionID) 
+);
+
+CREATE TABLE StudentExam (
+    StudentExamID INT PRIMARY KEY IDENTITY(1,1),
+    StudentID INT FOREIGN KEY REFERENCES Student(StudentID),
+    ExamID INT FOREIGN KEY REFERENCES Exam(ExamID),
+    StartTime DATETIME,
+    EndTime DATETIME,
+    TotalGrade INT 
+);
+
+CREATE TABLE StudentAnswer (
+    StudentAnswerID INT PRIMARY KEY IDENTITY(1,1),
+    StudentExamID INT FOREIGN KEY REFERENCES StudentExam(StudentExamID) ON DELETE CASCADE,
+    QuestionID INT FOREIGN KEY REFERENCES Question(QuestionID),
+    ChosenOptionID INT FOREIGN KEY REFERENCES [Option](OptionID) 
+);
